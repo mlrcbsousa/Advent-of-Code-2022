b107dcc8528d5c76322ac5bb3171f429ecd54fe5
Manuel Sousa <mlrcbsousa@gmail.com>
2022-12-17 18:48:33 +0000

    Day 5 Solution 1.

--- day-5/solution-1.ts
+++ day-5/solution-1.ts
@@ -1,14 +1,43 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
+const c = console.log
+
 const filePath = path.join(__dirname, 'input.txt');
 const file = fs.readFileSync(filePath, 'utf8');
 
-const lines = file.split('\n');
+const [head, procedure] = file.split('\n\n');
+const rows = head.split('\n');
+const steps = procedure.split('\n');
 
-lines.forEach(line => {
-  console.log(line);
+const crates: string[][] = [];
 
+rows.pop();
+rows.reverse()
+
+rows.forEach(row => {
+  let stack = 0;
+
+  row.split('').reduce(function(acc, crate, i) {
+    if (i % 4 === 1) {
+      acc[stack] ??= [];
+      if (crate !== ' ') {
+        acc[stack].push(crate);
+      }
+      stack++;
+    }
+    return acc;
+  }, crates)
 })
 
-console.log('result: ', 'foo');
+steps.forEach(function(step) {
+  const [n, from, to] = step.match(/\d+/g)!.map(Number);
+
+  for (let i = 0; i < n; i++) {
+    crates[to - 1].push(crates[from - 1].pop()!);
+  }
+});
+
+let result = crates.map((s) => s[s.length - 1]).join("");
+
+c(result);
